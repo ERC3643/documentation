9fb13beca717b8904be16cdb3331b8fd2b7223b6
kevin.thizy <kevin.thizy@intech.lu>
2023-10-05 10:35:57 +0200

    📝(style) Update colors and logos

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
 import styles from './index.module.css';
+import Logo from "../../static/img/erc3643.png";
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        <img src={Logo} alt="ERC3643 logo" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
